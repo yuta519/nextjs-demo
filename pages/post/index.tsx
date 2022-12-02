346353ee30ba99c0f510461d4d78505b15c57941
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-12-02 19:41:53 +0900

    fetch hacker

--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -1,5 +1,4 @@
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export async function getStaticProps() {
   const res = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty&limitToFirst=3&orderBy="$key"');
@@ -7,41 +6,34 @@ export async function getStaticProps() {
   return { props: { hackerRankTopIds } }
 }
 
-const User = (props: { hackerRankTopIds: any; }) => {
-
-  console.log("################################");
-  console.log("Top500のID一覧");
-  console.log(props.hackerRankTopIds);
-  console.log("################################");
-
-  const fetchCatFactsData = async (id: string) => {
-    const res = await fetch("https://hacker-news.firebaseio.com/v0/item/"+ id +".json?print=pretty");
-    const data = await res.json()
-    console.log("################################");
-    console.log("各記事の詳細情報");
-    console.log(data);
-    console.log("################################");
-    return data;
-  }
-
+const User = (props) => {
   useEffect(()=>{
-    props.hackerRankTopIds.forEach((id:string)=>{
-      fetchCatFactsData(id);
-    });
+    fetchArticleDetails(props.hackerRankTopIds);
   }, [props.hackerRankTopIds]);
-  const router = useRouter();
-  const id = router.query.id
 
-  const hackerNewsArticles = {
-      id: 1,
-      name: "hoge",
-    };
+  const [articles, setArticles] = useState([]);
 
+  const fetchArticleDetails = async (hackerRankTopIds) => {
+    hackerRankTopIds.forEach(async(id) => {
+      const res = await fetch("https://hacker-news.firebaseio.com/v0/item/"+ id +".json?print=pretty");
+      const data = await res.json();
+      // https://stackoverflow.com/questions/54676966/push-method-in-react-hooks-usestate
+      setArticles(prev => [...prev, data]);
+    });
+  }
 
   return (
     <>
-      <div>userId: {id}</div>
-      <div>{hackerNewsArticles.name}</div>
+      {console.log(articles)}
+
+      {articles.map((article) => {
+        return(
+          <>
+            <div>{article.id}</div>
+            <div>{article.title}</div>
+          </>
+        )
+      })}
     </>
   );
 }
