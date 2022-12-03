5b7488debcc99f726d932f39f53f6946ac6f92a7
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-12-03 14:05:36 +0900

    improve type

--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -6,17 +6,32 @@ export async function getStaticProps() {
   return { props: { hackerRankTopIds } }
 }
 
-const User = (props) => {
+type HackerRankeArticle = {
+  by: string;
+  descendants: number;
+  id: number;
+  kids: number[];
+  score: number;
+  text: string;
+  time: number;
+  title: string;
+  type: string;
+}
+
+const initialValue: HackerRankeArticle[] = [];
+
+const User = (props: { hackerRankTopIds: string[]; }) => {
   useEffect(()=>{
     fetchArticleDetails(props.hackerRankTopIds);
   }, [props.hackerRankTopIds]);
 
-  const [articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState(initialValue);
 
-  const fetchArticleDetails = async (hackerRankTopIds) => {
+  const fetchArticleDetails = async (hackerRankTopIds: string[]) => {
     hackerRankTopIds.forEach(async(id) => {
       const res = await fetch("https://hacker-news.firebaseio.com/v0/item/"+ id +".json?print=pretty");
-      const data = await res.json();
+      const data: HackerRankeArticle = await res.json();
+
       // https://stackoverflow.com/questions/54676966/push-method-in-react-hooks-usestate
       setArticles(prev => [...prev, data]);
     });
@@ -24,13 +39,12 @@ const User = (props) => {
 
   return (
     <>
-      {console.log(articles)}
-
       {articles.map((article) => {
         return(
           <>
             <div>{article.id}</div>
             <div>{article.title}</div>
+            <div>{article.by}</div>
           </>
         )
       })}
